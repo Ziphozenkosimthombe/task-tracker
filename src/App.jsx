1eda5e5cd80f0f70bc2b1a527fabe56bf777a590
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-06 17:43:13 +0200

    complete the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import  Tasks  from "./components/Tasks"
 import  AddTask  from "./components/AddTask"
+import Footer from "./components/Footer"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -22,11 +23,29 @@ function App() {
     return data           
   }
 
+  //Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data           
+  }
+
+  
+  
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
   }
 
   //Delete Task
@@ -40,10 +59,24 @@ function App() {
    
   }
   //Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updateTask = {...taskToToggle,
+    reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updateTask)
+    })
+    const data = await res.json()
+
+
     setTasks(tasks.map((task) =>
         task.id === id ? {...task, reminder:
-        !task.reminder} : task
+        data.reminder} : task
       )
     )
   }
@@ -58,7 +91,8 @@ function App() {
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
         'No Tasks To Show'
-      )}      
+      )}  
+      <Footer/>    
     </div>
     </>
   )
